fdeaca479a02ce4762ddc09f975adfff225d1a2f
kal1vane <prizrak41411@gmail.com>
2024-04-27 21:03:12 +0500

    add styled typography

---
+++ src/components/ui/StyledTypography/StyledTypography.tsx
@@ -0,0 +1,25 @@
+
+import { Typography } from '@mui/material';
+import styled from 'styled-components';
+
+// STYLED TYPOGRAPHY
+
+
+export const NormalText = styled(Typography)`
+   && {
+     font-family:FreeSet,sans-serif;
+     font-size:16px;
+     font-weight: 400;
+     color: #575756;
+   }
+`;
+ 
+ 
+export const Normal14Text = styled(Typography)`
+   && {
+     font-family:FreeSet,sans-serif;
+     font-size:14px;
+     font-weight: 400;
+     color: #575756;
+   }
+`;
