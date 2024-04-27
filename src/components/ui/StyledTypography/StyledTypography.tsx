776b5df6327bdaf9c513c21a1fbd081102c17ae9
kal1vane <prizrak41411@gmail.com>
2024-04-27 21:28:38 +0500

    fixed font style

--- src/components/ui/StyledTypography/StyledTypography.tsx
+++ src/components/ui/StyledTypography/StyledTypography.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 export const NormalText = styled(Typography)`
    && {
-     font-family:FreeSet,sans-serif;
+     font-family: Roboto, sans-serif;
      font-size:16px;
      font-weight: 400;
      color: #575756;
@@ -17,7 +17,7 @@ export const NormalText = styled(Typography)`
  
 export const Normal14Text = styled(Typography)`
    && {
-     font-family:FreeSet,sans-serif;
+     font-family:Roboto, sans-serif;
      font-size:14px;
      font-weight: 400;
      color: #575756;
